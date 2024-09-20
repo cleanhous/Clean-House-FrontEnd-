af29aca4a2b668413ed70529caa08845c31fa696
IsaaczZj <joseisac.isaac123@gmail.com>
2024-09-20 14:09:27 -0300

    continuando responsividade

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
       <div className="flex-1 ">
         <div className="md:flex md:flex-row-reverse md:p-10 ">
           <img
-            className="mx-auto px-4 mt-5 md:w-[550px] md:[550px]"
+            className="mx-auto px-4 mt-5 md:w-[450px] md:[450px]"
             src={logo}
             alt="Logo"
           />
@@ -44,7 +44,7 @@ const Home = () => {
               Praticidade, comodidade e segurança em um só aplicativo de
               serviços domésticos.
             </p>
-            <p className="hidden md:inline-block md:ml-5 md:text-white md:italic">
+            <p className="hidden md:inline-block md:ml-10 md:text-white md:italic md:-mt-10">
               "Tenha tudo em um só lugar, disponível a qualquer momento, com uma
               incrível qualidade de serviço"
             </p>
@@ -88,21 +88,28 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="mt-6 p-3 mx-4 flex flex-col justify-center items-center bg-white rounded-lg ">
-        <Handshake className="w-20 h-20 text-sky-700" />
-        <p className="font-bold text-xl text-sky-700">
-          Precisando de praticidade?
-        </p>
-        <p className="text-center p-4 font-semibold">
-          Estamos aqui para ajudar você a encontrar os melhores profissionais
-          com facilidade e confiança. Conte com a gente!
-        </p>
-        <button
-          className="w-auto p-3 text-white font-semibold rounded-xl bg-sky-700 hover:bg-sky-800"
-          onClick={handleClick}
-        >
-          Registre-se agora
-        </button>
+      <div className="mt-6 p-3 mx-4 flex flex-col justify-center items-center bg-white rounded-lg md:flex md:flex-row">
+        <Handshake className="md:hidden w-20 h-20 text-sky-700" />
+        <img
+          src="https://img.freepik.com/vetores-gratis/produtos-de-limpeza-com-produtos-de-limpeza-servico-de-limpeza_18591-52068.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1726531200&semt=ais_hybrid"
+          className="hidden md:block md:w-[450px] md:h-[450px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-center">
+          <p className="font-bold text-xl text-sky-700 md:text-4xl">
+            Precisando de praticidade?
+          </p>
+          <p className="text-center p-4 font-semibold md:text-xl">
+            Estamos aqui para ajudar você a encontrar os melhores profissionais
+            com facilidade e confiança. Conte com a gente!
+          </p>
+          <button
+            className="w-auto p-3 text-white font-semibold rounded-xl bg-sky-700 hover:bg-sky-800 md:w-72 "
+            onClick={handleClick}
+          >
+            Registre-se agora
+          </button>
+        </div>
       </div>
       <div className="bg-white mt-6 p-2">
         <p className=" text-center font-semibold text-xl text-sky-700">
@@ -110,7 +117,7 @@ const Home = () => {
         </p>
         <div className="bg-white grid grid-cols-3 grid-rows-3 mt-2 p-3 mx-4 gap-4">
           <div className="row-span-1 rounded-lg col-span-1 flex flex-col items-center shadow-lg bg-gray-200 p-4 text-sky-700 text-lg">
-            <p className="text-center">Eletricista</p>
+            <p className="text-center">Eletricista</p> 
             <PlugZap className="w-8 h-8 text-sky-600 mt-2" />
           </div>
           <div className="row-span-1 rounded-lg col-span-1 flex flex-col items-center shadow-lg bg-gray-200 p-4 text-sky-700 text-lg">
@@ -147,31 +154,32 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="mt-6 p-3 mx-4 mb-6 flex flex-col items-center bg-white rounded-lg">
-        <MessageCircleQuestion className="w-20 h-20 text-sky-700 " />
-        <p className="font-bold text-2xl text-sky-700">Quem somos?</p>
-        <p className="text-center p-4 font-semibold ">
-          Somos uma plataforma, onde você pode contratar serviços domésticos de
-          forma rápida, fácil e segura. Você só precisa escolher o serviço que
-          precisa, informar o seu endereço e o horário desejado, e pronto. Você
-          pode acompanhar o status do seu serviço pelo aplicativo.
-        </p>
+      <div className="mt-6 p-3 mx-4 mb-6 flex flex-col items-center bg-white rounded-lg md:flex-row md:justify-around md:h-64">
+        <div className="flex flex-col items-center md:flex-col">
+          <MessageCircleQuestion size={60} className=" text-sky-700 " />
+          <p className="font-bold text-2xl text-sky-700">Quem somos?</p>
+          <p className="text-center p-4 font-semibold ">
+            Somos uma plataforma, onde você pode contratar serviços domésticos
+            de forma rápida, fácil e segura.
+          </p>
+        </div>
+
         <div className="flex flex-col gap-3 p-5 justify-center items-center">
-          <Users className="text-sky-700" size={36} />
+          <Users className="text-sky-700" size={60} />
           <h5 className="text-xl font-bold text-sky-700">Faça seu chamado</h5>
           <p className="text-center font-semibold">
             Chame um profissional qualificado a qualquer momento
           </p>
         </div>
         <div className="flex flex-col gap-3 p-5 justify-center items-center">
-          <Award className="text-sky-700 " size={36} />
+          <Award className="text-sky-700 " size={60} />
           <h5 className="text-xl font-bold text-sky-700">Segurança</h5>
           <p className="text-center font-semibold">
             Um profissional qualificado irá atendê-lo
           </p>
         </div>
         <div className="flex flex-col gap-3 p-5 justify-center items-center">
-          <Phone className="text-sky-700" size={36} />
+          <Phone className="text-sky-700" size={60} />
           <h5 className="text-xl font-bold text-sky-700">Praticidade</h5>
           <p className="text-center font-semibold">
             Você poderá chamar um profissional de qualquer dispositivo conectado
