46b7fd37934ff186670a99d51172ffe2904d76af
IsaaczZj <joseisaac.isaac123@gmail.com>
2024-09-20 09:47:57 -0300

    inicio responsividade web

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import NavBar from "./NavBar";
 import logo from "/public/logo 1.svg";
+import eletricista from "/public/eletricista.jpg";
+import encanador from "/public/encanador.jpg";
+import diarista from "/public/diarista.jpg";
 import {
   PlugZap,
   Droplet,
@@ -29,34 +32,67 @@ const Home = () => {
   return (
     <div className="min-h-screen flex flex-col bg-sky-700">
       <NavBar />
-      <div className="flex-1">
-        <img className="mx-auto px-4 mt-5" src={logo} alt="Logo" />
-        <p className="p-5 text-center mt-4 text-xl text-white">
-          Praticidade, comodidade e segurança
-        </p>
-        <div className="w-full bg-white h-44 p-2">
-          <p className="text-center text-xl mb-4 text-sky-700 font-semibold">
+      <div className="flex-1 ">
+        <div className="md:flex md:flex-row-reverse md:p-10 ">
+          <img
+            className="mx-auto px-4 mt-5 md:w-[550px] md:[550px]"
+            src={logo}
+            alt="Logo"
+          />
+          <div className="md:flex md:flex-col md:p-4">
+            <p className="p-5 text-center mt-4 text-xl text-white md:text-3xl md:text-left md:py-16">
+              Praticidade, comodidade e segurança em um só aplicativo de
+              serviços domésticos.
+            </p>
+            <p className="hidden md:inline-block md:ml-5 md:text-white md:italic">
+              "Tenha tudo em um só lugar, disponível a qualquer momento, com uma
+              incrível qualidade de serviço"
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full bg-white h-48 p-2 md:p-8 md:h-auto">
+          <p className="text-center text-xl mb-4 text-sky-700 font-semibold md:text-4xl md:font-bold">
             Alguns de nossos serviços
           </p>
-          <div className="flex justify-between">
-            <div className="shadow-lg rounded-full w-28 h-28 bg-gray-200 p-4 flex flex-col justify-center items-center text-sky-700 text-lg">
-              Eletricista
-              <PlugZap className="w-8 h-8 text-sky-600 mt-2" />
+          <div className="flex justify-between md:justify-around">
+            <div className="shadow-lg rounded-full w-28 h-28 bg-gray-200 p-4 flex flex-col justify-center items-center  md:rounded-none md:w-auto md:h-72 md:p-0 md:bg-white md:shadow-2xl">
+              <p className="text-sky-700 text-lg md:mt-6 md:text-2xl">
+                Eletricista
+              </p>
+              <img className="hidden md:block w-96 h-auto" src={eletricista} />
+              <PlugZap className="md:hidden w-8 h-8 text-sky-600 mt-2" />
             </div>
-            <div className="shadow-lg rounded-full w-28 h-28 bg-gray-200 p-4 flex flex-col justify-center items-center text-sky-700 text-lg">
-              Encanador
-              <Droplet className="w-8 h-8 text-sky-600 mt-2" />
+            <div className="shadow-lg rounded-full w-28 h-28 bg-gray-200 p-4 flex flex-col justify-center items-center  md:rounded-none md:w-auto md:h-72 md:p-0 md:bg-white md:shadow-2xl">
+              <p className="text-sky-700 text-lg md:mt-6 md:text-2xl">
+                Encanador
+              </p>
+              <img
+                className="hidden md:block w-96 h-auto"
+                src={encanador}
+                alt="encanador"
+              />
+              <Droplet className="md:hidden w-8 h-8 text-sky-600 mt-2" />
             </div>
-            <div className="shadow-lg rounded-full w-28 h-28 bg-gray-200 p-4 flex flex-col justify-center items-center text-sky-700 text-lg">
-              Diarista
-              <User className="w-8 h-8 text-sky-600 mt-2" />
+            <div className="shadow-lg rounded-full w-28 h-28 bg-gray-200 p-4 flex flex-col justify-center items-center  md:rounded-none md:w-auto md:h-72 md:p-0 md:bg-white md:shadow-2xl">
+              <p className="text-sky-700 text-lg md:mt-6 md:text-2xl">
+                Diarista
+              </p>
+              <img
+                className="hidden md:block w-96 h-auto"
+                src={diarista}
+                alt=""
+              />
+              <User className="md:hidden w-8 h-8 text-sky-600 mt-2" />
             </div>
           </div>
         </div>
       </div>
       <div className="mt-6 p-3 mx-4 flex flex-col justify-center items-center bg-white rounded-lg ">
         <Handshake className="w-20 h-20 text-sky-700" />
-        <p className="font-bold text-xl text-sky-700">Precisando de praticidade?</p>
+        <p className="font-bold text-xl text-sky-700">
+          Precisando de praticidade?
+        </p>
         <p className="text-center p-4 font-semibold">
           Estamos aqui para ajudar você a encontrar os melhores profissionais
           com facilidade e confiança. Conte com a gente!
@@ -123,17 +159,24 @@ const Home = () => {
         <div className="flex flex-col gap-3 p-5 justify-center items-center">
           <Users className="text-sky-700" size={36} />
           <h5 className="text-xl font-bold text-sky-700">Faça seu chamado</h5>
-          <p className="text-center font-semibold">Chame um profissional qualificado a qualquer momento</p>
+          <p className="text-center font-semibold">
+            Chame um profissional qualificado a qualquer momento
+          </p>
         </div>
         <div className="flex flex-col gap-3 p-5 justify-center items-center">
           <Award className="text-sky-700 " size={36} />
           <h5 className="text-xl font-bold text-sky-700">Segurança</h5>
-          <p className="text-center font-semibold">Um profissional qualificado irá atendê-lo</p>
+          <p className="text-center font-semibold">
+            Um profissional qualificado irá atendê-lo
+          </p>
         </div>
         <div className="flex flex-col gap-3 p-5 justify-center items-center">
           <Phone className="text-sky-700" size={36} />
           <h5 className="text-xl font-bold text-sky-700">Praticidade</h5>
-          <p className="text-center font-semibold">Você poderá chamar um profissional de qualquer dispositivo conectado a internet</p>
+          <p className="text-center font-semibold">
+            Você poderá chamar um profissional de qualquer dispositivo conectado
+            a internet
+          </p>
         </div>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,13 +16,16 @@ const NavBar = () => {
       <button onClick={handleClickHome} className="w-10 h-auto">
         <img src={logo} alt="" />
       </button>
+      <div className="space-x-8">
+        <button className="text-sky-700">FAQ</button>
 
-      <button
-        className="w-auto p-3 text-white font-semibold rounded-full bg-sky-600 hover:bg-sky-700"
-        onClick={handleClick}
-      >
-        Entrar
-      </button>
+        <button
+          className="w-auto p-3 text-white font-semibold rounded-full bg-sky-600 hover:bg-sky-700"
+          onClick={handleClick}
+        >
+          Entrar
+        </button>
+      </div>
     </div>
   );
 };
